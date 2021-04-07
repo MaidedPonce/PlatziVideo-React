32cb39bf4620fae3016b3736fc7d7d800189b68b
Maided <maided.h.p90@gmail.com>
2021-04-07 15:05:59 -0500

    agregando una configuracion para enrutamiento más accesible

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, compose } from 'redux'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
 import reducer from './reducers'
 import App from './routes/App'
 // reducer viene de la carpeta de reducers que creamos y del archivo index
@@ -171,6 +173,8 @@ const initialState = {
     }
   ]
 }
+
+const history = createBrowserHistory()
 // conexion a devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
 // referencia a nuestro store. Crear un nuevo store para poderselo pasar a nuestro provider. Va a utilizar a createStore y va a recibir dos parámetros: el primero un reducer y el segundo el estado inicial
@@ -178,6 +182,8 @@ const store = createStore(reducer, initialState, composeEnhancers())
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history>={history}
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app'))
